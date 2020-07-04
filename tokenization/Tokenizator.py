0b972e0b519024e47b3677eb7658e4eca0bb1434
wencai.xu <wencai.xu@ipinyou.com>
2020-07-04 18:50:55 +0800

    style:添加ReadMe

--- tokenization/Tokenizator.py
+++ tokenization/Tokenizator.py
@@ -2,5 +2,5 @@ from setting.stop_words_setting import STOP_WORDS, DOCUMENT_WORDS
 
 
 def tokenize(content):
-    WORDS = set(DOCUMENT_WORDS.lower().split(" "))
-    return WORDS - STOP_WORDS
+    words = set(DOCUMENT_WORDS.lower().split(" "))
+    return words - STOP_WORDS
